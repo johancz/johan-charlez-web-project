c23c740f87f967e7ef1f5f8d1bb20a0ed9aae548
JoCh <johancz@users.noreply.github.com>
2021-02-22 11:59:47 +0100

    Fetch pages' script files instead of adding them by URI.
    - Add comments to and cleanup "script.js" a little.
    - Enable "Logout"-button on the page: "page-signedin.html".

--- src/scripts.js
+++ src/scripts.js
@@ -11,7 +11,7 @@
     "about-developer": document.getElementById("page-about-developer"),
     "main-contents": document.getElementById("page-main-contents")
   };
-  const [ $mainContainer, $mainOtherPages ] = document.getElementsByTagName("main");
+  const [$mainContainer, $mainOtherPages] = document.getElementsByTagName("main");
   const $pageScriptsContainer = document.getElementById("page-scripts");
 
   /* Random Fox API */
@@ -25,8 +25,8 @@
   e: the Event
   */
   function handleClick_mainMenu(e) {
+    // Stop the browser from loading 'e.target.href'.
     e.preventDefault();
-    console.log("clicked menu", e.target.dataset);
 
     // Show the correct page and hide the rest.
     switch (e.target.dataset.menuitem) {
@@ -38,42 +38,51 @@
         $mainContainer.classList.remove("hidden");
         break;
       case "AboutTheDeveloper":
-      case "Login":
+      case "Login": {
         // Remove old page scripts:
         while ($pageScriptsContainer.firstChild !== null) {
           $pageScriptsContainer.removeChild($pageScriptsContainer.firstChild);
         }
 
+        // todo(joch): use await instead?
+        // Fetch the page.
         fetch(e.target.href).then(response => {
+          // Success, await the data and insert the page's markup.
           response.text().then(data => {
             $mainOtherPages.innerHTML = data;
 
             // Once the new page has been added to the DOM, load its' scripts (if any).
             if (e.target.dataset.pageScripts) {
               for (let scriptPath of JSON.parse(e.target.dataset.pageScripts)) {
-                let $script = document.createElement("script");
-                $script.type = "text/javascript";
-                $script.src = scriptPath;
-                $pageScriptsContainer.appendChild($script);
+
+                // Fetch all of the page's script-files using provided scriptPaths.
+                fetch(scriptPath).then(response => {
+
+                  // Success, await the data and add the scripts to <body>/$pageScriptsContainer.
+                  response.text().then(data => {
+                    let $script = document.createElement("script");
+                    $script.type = "text/javascript";
+                    $script.textContent = data;
+                    $pageScriptsContainer.appendChild($script);
+                  });
+                });
               } 
             }
-            
+
             $mainContainer.classList.add("hidden");
             $mainOtherPages.classList.remove("hidden");
-          });
-        });
+          }); // end of 'response.text().then()'
+        }); // end of 'fetch'
         break;
+      } // end of 'case: "Login"'
       default:
         console.error(`The "${e.target.dataset.menuitem}"-menuitem was unexpected. It does not have a corresponding "page".`);
         break;
     }
   }
 
-  /*
-  e: the Event
-  */
+  // note(joch): If there is only one event to handle, remove this function and reference the 'handleClick_mainMenu'-callback directly?
   function handleEvent(e) {
-    console.log(e);
     switch (e.type) {
       case "click":
         if (e.currentTarget.id === "nav-main") {
